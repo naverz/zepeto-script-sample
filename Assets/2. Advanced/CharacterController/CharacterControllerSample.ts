420afc4b64abddac5fb2a96231b394b82bf12a3c
heejun <jason.k@snowcorp.com>
2021-10-19 13:08:06 +0900

    sample code update
    zepeto loading by zepetoid

--- Assets/2. Advanced/CharacterController/CharacterControllerSample.ts
+++ Assets/2. Advanced/CharacterController/CharacterControllerSample.ts
@@ -1,14 +1,25 @@
 import { ZepetoScriptBehaviour } from 'ZEPETO.Script'
 import {SpawnInfo, ZepetoPlayers} from 'ZEPETO.Character.Controller'
+import {Quaternion, Vector3} from "UnityEngine"
 
 export default class CharacterController extends ZepetoScriptBehaviour {
 
     Start() {
-        ZepetoPlayers.instance.CreatePlayerWithHashCode("", "F8URQA", new SpawnInfo(), true);
-        // ZepetoPlayers.instance.CreatePlayerWithZepetoId("", "gamedev1", new SpawnInfo(), true)
+        // set character spawn location
+        var spawninfo = new SpawnInfo();
+        spawninfo.position = Vector3.zero;
+        spawninfo.rotation = Quaternion.identity;
         
+        // load charater zepeto id
+        ZepetoPlayers.instance.CreatePlayerWithZepetoId("", "gamedev1", spawninfo, true);
+        
+        // add init callback
         ZepetoPlayers.instance.OnAddedLocalPlayer.AddListener(() => {
             let _player = ZepetoPlayers.instance.LocalPlayer;
+            // test jump
+            _player.Jump();
+            // debug log
+            console.log(` player created -> ${_player.name}` );
         });
     }
 
